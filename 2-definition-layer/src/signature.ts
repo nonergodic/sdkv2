a25f1a419cc1dd8f68bf614d0ee5b6a0f8d1884b
nonergodic <nonergodic@gmail.com>
2023-07-30 02:20:08 -0700

    remove address stub and use actual package now that build works

--- 2-definition-layer/src/signature.ts
+++ 2-definition-layer/src/signature.ts
@@ -2,16 +2,10 @@
 //  internals and can swap out the implementation if we choose to later. Maybe also rename
 //  recovery to v then (this seems to be the convention at least in EVM land)
 
-//TODO getting unknown file extension .ts when including this package
-// import { Signature as SignatureOptionalRecovery } from "@noble/secp256k1";
+import { Signature as SignatureOptionalRecovery } from "@noble/secp256k1";
 
-// export class Signature extends SignatureOptionalRecovery {
-//   constructor(readonly r: bigint, readonly s: bigint, readonly recovery: number) {
-//     super(r, s, recovery);
-//   }
-// }
-
-//TODO remove!
-export class Signature {
-  constructor(readonly r: bigint, readonly s: bigint, readonly recovery: number) {}
+export class Signature extends SignatureOptionalRecovery {
+  constructor(readonly r: bigint, readonly s: bigint, readonly recovery: number) {
+    super(r, s, recovery);
+  }
 }
